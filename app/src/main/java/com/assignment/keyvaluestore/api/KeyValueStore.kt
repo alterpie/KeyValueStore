88cff92bc520280447bc15a69a967831315c6867
oleksii.yerastov <oleksii.yerastov@gmail.com>
2022-12-19 21:53:41 +0100

    Add api interface

---
+++ app/src/main/java/com/assignment/keyvaluestore/api/KeyValueStore.kt
@@ -0,0 +1,33 @@
+package com.assignment.keyvaluestore.api
+
+interface KeyValueStore {
+
+    /**
+     * store the value for key
+     */
+    fun set(key: String, value: String)
+    /**
+     * return the current value for key
+     */
+    fun get(key: String): String?
+    /**
+     * remove the entry for key
+     */
+    fun delete(key: String)
+    /**
+     * return the number of keys that have the given value
+     */
+    fun count(value: String): Int
+    /**
+     * start a new transaction
+     */
+    fun begin()
+    /**
+     * complete the current transaction
+     */
+    fun commit()
+    /**
+     * revert to state prior to BEGIN call
+     */
+    fun rollback()
+}
--- app/src/main/java/com/assignment/keyvaluestore/ui/MainActivity.kt
+++ app/src/main/java/com/assignment/keyvaluestore/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.assignment.keyvaluestore
+package com.assignment.keyvaluestore.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
